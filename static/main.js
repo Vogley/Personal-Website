4406969928776cdc06b667bc8ad340fe7edabfd3
Vogley <46629178+Vogley@users.noreply.github.com>
2019-09-18 19:54:20 -0400

    Mobile Rollout

--- static/main.js
+++ static/main.js
@@ -1,3 +1,4 @@
+//Animation helper library
 AOS.init();
 
 //Observer for Animations
@@ -6,14 +7,15 @@ let options = {
   rootMargin: '0px',
   threshold: 0.2
 }
-let animated = false
-console.log(window.screenX);
+let animated = false;
+let mobile = (screen.width <= 640);
+
 
 let callback = (entries, observer) => { 
     entries.forEach(entry => {
-        if(entry.isIntersecting && !animated) {
+        if(entry.isIntersecting && !animated && !mobile) {
+            console.log("hello");
             $(".skill-bar > span").each(function() {
-                console.log($(this).width()/$("#skills").width());
                 $(this)
                     .data("origWidth", $(this).width()/$("#skills").width())
                     .width(0)
@@ -21,6 +23,7 @@ let callback = (entries, observer) => {
                     width: $(this).data("origWidth")*200 + "%" // or + "%" if fluid
                     }, 1200);
             });
+
             animated = true;
         }
     });
@@ -34,11 +37,49 @@ observer.observe(target);
 
 //Pop-Up function
 function infoPopUp() {
-    var popup = document.getElementById("myPopup");
+    let popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
 
 function infoPopOff() {
-    var popup = document.getElementById("myPopup");
+    let popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+}
+
+
+//Toggle Bio
+function openBio() {
+    let bio = document.getElementById("bio");
+    let openBio = document.getElementById("openBio");
+    bio.style.display = "block";
+    openBio.style.display = "none";
+}
+
+//Toggle Skills
+let skillsShow = false;
+function toggleSkills() {
+    let skills = document.getElementById("skills");
+    let arrow = document.getElementById("arrow");
+    if(skillsShow) {
+        skills.style.height = "0px";
+        let children = skills.childNodes;
+        for(let i = 0; i < 3; i++) {
+            children[2*i + 1].style.display = "none";
+        }
+    }
+    else  {
+        skills.style.height = "530px";
+        let children = skills.childNodes;
+        for(let i = 0; i < 3; i++) {
+            children[2*i + 1].style.display = "block";
+        }
+        let col = document.getElementById("observation");
+        mobile = false;
+        observer.observe(col);
+        
+    }
+
+    arrow.classList.toggle("fa-angle-down");
+    arrow.classList.toggle("fa-angle-up");
+    skillsShow = !skillsShow;
 }
